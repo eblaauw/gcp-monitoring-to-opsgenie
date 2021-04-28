bd3cfe5855b54515af0ee3ce0d50e5c58854db22
Edzo Blaauw <52958296+edzotalabat@users.noreply.github.com>
2021-04-28 11:45:04 +0400

    Add OpsGenie webhook

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # GCP Alert to OpsGenie
 
-Convert a GCP monitoring alert to OpsGenie using a cloudfunction
+Convert a GCP monitoring alert to OpsGenie using a Cloud Function
 
 ## Installation
 
 Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
 
 ```bash
-pip install requirements.txt -r 
+pip install src/requirements.txt -r
 ```
 
 ## Testing the function locally
@@ -20,12 +20,14 @@ pip install requirements.txt -r
 curl --header "Content-Type: application/json" \
   --request POST \
   --data @../test/test.json \
-  http://localhost:8080
+  http://localhost:8080\?authentication_token\=default_token
 ```
 
+## Deploying the Cloud Function to Google Cloud
+Refer to [this](https://cloud.google.com/functions/docs/deploying#deployment_options) article on how to deploy a Cloud Function 
 
 ## To do
-- [ ] Create opsgenie hook
+- [x] Create opsgenie hook
 - [ ] Creat deployment guidebook
 
 ## License
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from flask import abort
 import os
+import requests
+import json
 
 AUTHENTICATION_TOKEN = os.environ.get("AUTHENTICATION_TOKEN", "default_token")
-OPSGENIE_WEBHOOK_URL = os.environ.get("OPSGENIE_WEBHOOK_URL", "")
-OPSGENIE_API_TOKEN = os.environ.get("OPSGENIE_API_TOKEN", "")
+OPSGENIE_WEBHOOK_URL = os.environ.get(
+    "OPSGENIE_WEBHOOK_URL", "https://api.opsgenie.com/v2/alerts"
+)
+OPSGENIE_API_TOKEN = os.environ.get(
+    "OPSGENIE_API_TOKEN", ""
+)
 
 
 def verify_token(given_token):
@@ -32,18 +38,35 @@ def handler(request):
     if request.method == "POST":
         if verify_token(request.args.get("authentication_token")):
             json_object = request.get_json()
-            opsgenie_object = {
-                "message": json_object["incident"]["summary"],
-                "alias": "Alias",
-                "description": "GCP alert converted message",
-                "priority": "P1",
-            }
-            return send_opsgenie_alert(opsgenie_object)
+            return send_opsgenie_alert(json_object)
         else:
             return abort(405)
     return abort(405)
 
 
-def send_opsgenie_alert(opsgenie_object):
-    print(opsgenie_object)
-    return "OK"
+def send_opsgenie_alert(json_object):
+    message = str(
+        "[GCP Message]: "
+        + json_object["incident"]["condition_name"]
+        + " "
+        + json_object["incident"]["resource_name"]
+    )
+    headers = {
+        "Authorization": "GenieKey " + OPSGENIE_API_TOKEN,
+        "Content-Type": "application/json",
+    }
+    opsgenie_object = {
+        "message": message,
+        "alias": "Alias",
+        "description": json_object["incident"]["summary"],
+        "priority": "P1",
+    }
+    try:
+        re = requests.post(
+            OPSGENIE_WEBHOOK_URL, data=json.dumps(opsgenie_object), headers=headers
+        )
+        re.raise_for_status()
+        return "OK"
+    except Exception as exception:
+        print(exception)
+        raise
